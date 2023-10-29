0f9142a0931bd6865038edc2211c0421546354af
DanieLSBTH <josuemarque15@gmail.com>
2023-10-28 22:39:27 -0600

    k

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,19 +1,16 @@
-module.exports = {
-  HOST: "ep-quiet-math-78680175-pooler.us-east-1.postgres.vercel-storage.com",
-  USER: "default",
-  PASSWORD: "fTjywm5J3oZD",
-  DB: "verceldb",
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('verceldb', 'default', 'fTjywm5J3oZD', {
+  host: 'ep-quiet-math-78680175-pooler.us-east-1.postgres.vercel-storage.com',
+  dialect: 'postgres',
+  port: 5432, // Asegúrate de que el puerto sea correcto
   dialectOptions: {
     ssl: {
       require: true,
-      // Puedes agregar más opciones SSL aquí si es necesario
-    }
-  }
-};
+    },
+  },
+});
+
+// Resto de tu código para definir modelos y realizar consultas
+
+module.exports = sequelize;
