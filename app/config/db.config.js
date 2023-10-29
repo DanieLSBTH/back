ee8bc9d6bc87bbdd1df5406d6c52635a68fd7bd4
DanieLSBTH <josuemarque15@gmail.com>
2023-10-28 22:58:55 -0600

    F

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  URL:"postgres://default:fTjywm5J3oZD@ep-quiet-math-78680175-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
-  PRISMA_URL:"postgres://default:fTjywm5J3oZD@ep-quiet-math-78680175-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15",
-  URL_NON_POOLING:"postgres://default:fTjywm5J3oZD@ep-quiet-math-78680175.us-east-1.postgres.vercel-storage.com:5432/verceldb",
-  
   HOST: "ep-quiet-math-78680175-pooler.us-east-1.postgres.vercel-storage.com",
   USER: "default",
   PASSWORD: "fTjywm5J3oZD",
