195b32d4024eecdc855dd36857fcb2f91a76bd9b
DanieLSBTH <josuemarque15@gmail.com>
2023-10-28 22:44:31 -0600

    bd2

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,16 +1,19 @@
-const { Sequelize } = require('sequelize');
+module.exports = {
+  URL:"postgres://default:fTjywm5J3oZD@ep-quiet-math-78680175-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+  PRISMA_URL:"postgres://default:fTjywm5J3oZD@ep-quiet-math-78680175-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15",
+  URL_NON_POOLING:"postgres://default:fTjywm5J3oZD@ep-quiet-math-78680175.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+  
+  HOST: "ep-quiet-math-78680175-pooler.us-east-1.postgres.vercel-storage.com",
+  USER: "default",
+  PASSWORD: "fTjywm5J3oZD",
+  DB: "verceldb",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
 
-const sequelize = new Sequelize('verceldb', 'default', 'fTjywm5J3oZD', {
-  host: 'ep-quiet-math-78680175-pooler.us-east-1.postgres.vercel-storage.com',
-  dialect: 'postgres',
-  port: 5432, // Asegúrate de que el puerto sea correcto
-  dialectOptions: {
-    ssl: {
-      require: true,
-    },
-  },
-});
 
-// Resto de tu código para definir modelos y realizar consultas
-
-module.exports = sequelize;
