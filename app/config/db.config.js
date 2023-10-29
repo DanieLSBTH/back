fb6e1527363b4ada60004173cb7a7eea52d08da4
DanieLSBTH <josuemarque15@gmail.com>
2023-10-28 23:11:15 -0600

    l

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "dpg-ckrfeqm2eoec73ffjql0-a.oregon-postgres.render.com/umg_antigua",
+  HOST: "dpg-ckrfeqm2eoec73ffjql0-a.oregon-postgres.render.com",
   USER: "umg_antigua_user",
   PASSWORD: "U6mQANExZDR0XSp5fsB5LG7m14Ldz2o6",
   DB: "umg_antigua",
