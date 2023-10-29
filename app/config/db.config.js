69a1d7077c2a0c4db610ca7001da1dcea6e3c44d
DanieLSBTH <josuemarque15@gmail.com>
2023-10-28 22:29:22 -0600

    df

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "ep-quiet-math-78680175.us-east-1.postgres.vercel-storage.com",
+  HOST: "ep-quiet-math-78680175-pooler.us-east-1.postgres.vercel-storage.com",
   USER: "default",
   PASSWORD: "fTjywm5J3oZD",
   DB: "verceldb",
