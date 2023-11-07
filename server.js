9b5818a78253980e3f0e135440570faed009c2c8
DanieLSBTH <josuemarque15@gmail.com>
2023-11-07 14:57:45 -0600

    DDD

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const app = express();
 
 var corsOptions = {
-  origin: "https://tu-farmacia-en-linea.onrender.com"
+  origin: "http://192.168.1.15:3000"
 };
 
 app.use(cors(corsOptions));
